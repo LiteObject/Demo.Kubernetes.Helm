805444c873aa0120341887ca87c5282922a80a7f
LiteObject <Lite.Object@gmail.com>
2022-02-08 21:54:18 -0600

    multiple updates

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Helm Chart
+
+## To build docker image:
+$`docker build -f WeatherForecast.API/Dockerfile -t liteobject/demo-helm-chart:latest . --label "created-by=mhoque"
+
+## To view images details:
+$`docker inspect [NAME|ID]
+
+## To push to dockerhub
+$`docker push liteobject/demo-helm-chart:latest
+
+## To run using kubernetes deployment file:
+$`kubectl apply -f k8s/deployment.yaml
+
+## To delete a deployment:
+$`kubectl delete -f k8s/deployment.yaml
+
+## To watch logs using label:
+$`kubectl logs -l app=weather-forecast-webapi -f --since=10m
+
+## To access and interact with internal Kubernetes cluster processes from localhost
+$` k port-forward [pod-name] 8080:80
+
+## Install existing applications with Helm:
+- https://docs.microsoft.com/en-us/azure-stack/aks-hci/kubernetes-helm
+
+## To add the Nginx repo to your cluster using helm
+$`helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx 
+$`helm repo update
+
+## To install the Nginx ingress in the namespace ingress
+$`helm install my-nginx-ingress ingress-nginx/ingress-nginx `
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux `
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
+
--- WeatherForecast.API/Controllers/WeatherForecastController.cs
+++ WeatherForecast.API/Controllers/WeatherForecastController.cs
@@ -8,26 +8,33 @@ namespace WeatherForecast.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+            this._logger.LogInformation("\"{name}\" has been instantiated.", nameof(WeatherForecastController));
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            this._logger.LogInformation("\"{name}\" method has been invoked.", nameof(Get));
+
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+
+            this._logger.LogDebug("Result from \"{method}\" method:\n {result}", nameof(Get), System.Text.Json.JsonSerializer.Serialize(result));
+
+            return result;
         }
     }
 }
